b7853ae3c11a06e46e6186b3f3139c8fc57d2295
AroshaRavishan <mgaravishan@gmail.com>
2024-08-10 10:21:25 +0530

    Hello World

---
+++ Sqare Game/Sqare Game/Sqare_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sqare_GameApp.swift
+//  Sqare Game
+//
+//  Created by COBSCCOMPY231P-012 on 2024-08-10.
+//
+
+import SwiftUI
+
+@main
+struct Sqare_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
